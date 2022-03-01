b0646bfb86e9cf1c940250401519a84410958136
i346327 <rohith.m.n@sap.com>
2022-03-01 20:31:44 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.3.0"
-    }
-  }
+
+locals {
+  name = "rohith"
 }
 
-provider "aws" {
-  # Configuration options
-  region = "sa-east-1"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+# Create a VPC
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
