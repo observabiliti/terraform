d9b360e5aa509765ab2f5231d2aa4265e049d10a
i346327 <rohith.m.n@sap.com>
2022-03-10 00:18:54 +0530

    create ec2 with s3 access policy attached

--- 07-resource-meta-args/main.tf
+++ 07-resource-meta-args/main.tf
@@ -1,1 +1,58 @@
+terraform {
+  backend "remote" {
+      organisation = "krishna01"
+      workspace {
+          name = "terraform-work"
+      }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.3.0"
+    }
+  }
+}
 
+provider "aws" {
+    region = var.region
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+}
+
+resource "aws_iam_role" "ec2_s3_access_role" {
+    name = "s3-role"
+    assume_role_policy = "${file("assume_role_policy.json")}"  
+}
+
+resource "aws_iam_policy" "s3-policy" {
+    name = "s3-policy"
+    policy = "${file("policys3bucket.json")}"
+}
+
+resource "aws_iam_policy_attachment" "attach-policy" {
+    name =  "attach-policy"
+    roles = ["${aws_iam_role.ec2_s3_access_role.name}"]
+    policy_arn = "${aws_iam_policy.s3-policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "aws-instance-profile" {
+    name = "aws-instance-profile"
+    roles = ["${aws_iam_role.ec2_s3_access_role.name}"]
+}
+
+resource "aws_instance" "my-vms" {
+    ami = "ami-076754bea03bde973"
+    instance_type = "t2.micro"
+    iam_instance_profile = "${aws_iam_instance_profile.aws-instance-profile.name}"
+    count = 2
+    tags = {
+        Name = "server - ${count.index}"
+    }
+    depends_on = [
+        aws_iam_instance_profile.aws-instance-profile,
+    ]
+}
+
+output "instance_public_ips" {
+    value = "${join(",",aws_instance.my-vms.*.public_ip)}"
+}
---
+++ 07-resource-meta-args/variables.tf
@@ -0,0 +1,11 @@
+variable "region_name" {
+  
+}
+
+variable "AWS_ACCESS_KEY" {
+  
+}
+
+variable "AWS_SECRET_KEY" {
+  
+}
